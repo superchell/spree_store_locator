5b6c08203395933d554ad0070311e4a85697a192
Gonzalo Moreno <gmoreno@acid.cl>
2016-01-22 12:13:08 -0300

    add large image to store_locator

--- app/models/spree/store_locator.rb
+++ app/models/spree/store_locator.rb
@@ -4,8 +4,7 @@ class Spree::StoreLocator < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { large: "500x500>", medium: "200x200>", thumb: "100x100>" }
 
   validates :address1, :city, :country, :state, presence: true
 
