b560e197d141f0599c908526111101c35752dc53
Gonzalo Moreno <gmoreno@acid.cl>
2016-03-16 15:11:09 -0300

    refactor code

--- app/controllers/spree/admin/store_locators_controller.rb
+++ app/controllers/spree/admin/store_locators_controller.rb
@@ -1,64 +1,9 @@
 module Spree
   class Admin::StoreLocatorsController < Admin::ResourceController
-    def index
-      load_store_locators
-      @store_locator = Spree::StoreLocator.new
-    end
-
-    def new
-      @store_locator = Spree::StoreLocator.new
-    end
-
-    def create
-      @store_locator = Spree::StoreLocator.new store_params
-      if @store_locator.save
-        redirect_to admin_store_locators_path
-      else
-        flash[:error] = 'There was an error.'
-        render :new
-      end
-    end
-
-    def update
-      @store_locator.update_attributes store_params
-      redirect_to admin_store_locators_path
-    end
-
-    def destroy
-      @store_locator.destroy
-
-      flash[:success] = Spree.t('store_locator.deleted')
-
-      respond_with(@store_locator) do |format|
-        format.html { redirect_to collection_url }
-        format.js  { render_js_for_destroy }
-      end
-    end
-
     private
-
-    def load_store_locators
-      per_page = params[:per_page] || 20
-      @store_locators = Spree::StoreLocator.state_ordered.page(params[:page]).per(per_page)
-    end
 
     def find_resource
       @store_locator = Spree::StoreLocator.friendly.find params[:id]
-    end
-
-    def store_params
-      params.require(:store_locator).permit(
-          :address1,
-          :address2,
-          :city,
-          :country,
-          :image,
-          :name,
-          :phone,
-          :state,
-          :website,
-          :zip
-      )
     end
   end
 end
--- app/models/spree/store_locator.rb
+++ app/models/spree/store_locator.rb
@@ -1,28 +1,30 @@
 require 'geocoder'
 
-class Spree::StoreLocator < ActiveRecord::Base
-  extend FriendlyId
-  friendly_id :name, use: :slugged
+module Spree
+  class StoreLocator < ActiveRecord::Base
+    extend FriendlyId
+    friendly_id :name, use: :slugged
 
-  has_attached_file :image, styles: { large: "500x500>", medium: "200x200>", thumb: "100x100>" }
+    has_attached_file :image, styles: { large: "500x500>", medium: "200x200>", thumb: "100x100>" }
 
-  validates :address1, :city, :country, :state, presence: true
+    validates :address1, :city, :country, :state, presence: true
 
-  scope :state_ordered, -> { order('state ASC') }
+    scope :state_ordered, -> { order('state ASC') }
 
-  geocoded_by :full_street_address
+    geocoded_by :full_street_address
 
-  after_validation :geocode, :if => :has_location?, :unless => :has_coordinates?
+    after_validation :geocode, :if => :has_location?, :unless => :has_coordinates?
 
-  def full_street_address
-    "#{address1}, #{city}, #{state} #{zip}, #{country}"
-  end
+    def full_street_address
+      "#{address1}, #{city}, #{state} #{zip}, #{country}"
+    end
 
-  def has_coordinates?
-    latitude && longitude
-  end
+    def has_coordinates?
+      latitude && longitude
+    end
 
-  def has_location?
-    full_street_address.present?
+    def has_location?
+      full_street_address.present?
+    end
   end
 end
