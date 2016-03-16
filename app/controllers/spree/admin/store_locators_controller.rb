4976e18248b572946fd8578c8d764eaaf9372360
Gonzalo Moreno <gmoreno@acid.cl>
2016-03-16 15:17:38 -0300

    remove useless test

--- app/controllers/spree/admin/store_locators_controller.rb
+++ app/controllers/spree/admin/store_locators_controller.rb
@@ -1,5 +1,10 @@
 module Spree
   class Admin::StoreLocatorsController < Admin::ResourceController
+    def index
+      @search = Spree::StoreLocator.ransack(params[:q])
+      @collection = @search.result(distinct: true).page(params[:page]).per(Spree::Config[:admin_products_per_page])
+    end
+
     private
 
     def find_resource
