44fe850c843199d7eb2f98f45e6480f982e619df
Oleg Mozolev <superchell@users.noreply.github.com>
2016-10-26 10:58:13 +0300

    Add jquery lib

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( lib/handlebars-1.0.0.js lib/jquery.storelocator.js )
+Rails.application.config.assets.precompile += %w( lib/jquery.min.js lib/handlebars-1.0.0.js lib/jquery.storelocator.js )
